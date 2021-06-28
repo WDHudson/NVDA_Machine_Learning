a2a7a13bbe3ca92c9429b37aa6f052cfa50e5d76
William Hudson <williamdhudson@gmail.com>
2021-06-28 12:58:28 -0400

    Reads the csv file

--- machine_learning.py
+++ machine_learning.py
@@ -1,8 +1,15 @@
 # Install dependencies
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecesionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+plt.style.use('bmh')
 
+# Store the data into a dataframe
+df = pd.read_csv('NVDA.csv')
+
+# Get the number of trading days in the data set
+
+print(df.shape())
