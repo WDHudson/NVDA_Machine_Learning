b5ed95f0682631602bf671662e3311d7375f02d3
William Hudson <williamdhudson@gmail.com>
2021-06-28 12:50:47 -0400

    Installed dependencies

---
+++ machine_learning.py
@@ -0,0 +1,8 @@
+# Install dependencies
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecesionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
